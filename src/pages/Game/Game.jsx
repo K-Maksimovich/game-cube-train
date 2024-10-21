5bb4b0e89c0ffa14da5af4a27f90cd36eb45db00
km <carpovmax04@gmail.com>
2024-10-21 19:22:09 +0500

    commit second

---
+++ src/pages/Game/Game.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./game.css";
+
+const Game = () => {
+    return (
+        <div>
+            <h1>Game page</h1>
+        </div>
+    );
+};
+
+export default Game;
